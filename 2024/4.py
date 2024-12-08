7daccacdcfa964ce8a7b8724026e0387828963da
RickForce <Rick@Rickforce.com>
2024-12-08 11:28:38 +0100

    4b bruteforce

--- 2024/4.py
+++ 2024/4.py
@@ -1,3 +1,5 @@
+from sympy.logic.boolalg import Boolean
+
 from util import console, parse_file_as_list, time_function, convert_str_list_to_int_list
 import numpy as np
 import re
@@ -40,12 +42,37 @@ def run_a(file: list[str]):
 
 @time_function()
 def run_b(file: list[str]):
-    pass
+    xmas_map = np.array([list(line) for line in file])
+
+    # slice array into 3,3 blocks and see if X-MAS could be fitted in there
+    counter = 0
+    for y_index in range(len(xmas_map) - 2):
+        for x_index in range(len(xmas_map) -2):
+            three_by_three_block = xmas_map[y_index: y_index + 3, x_index: x_index + 3]
+            if check_is_xmas_block(three_by_three_block):
+                counter += 1
+    return counter
+
+
+
+def check_is_xmas_block(three_by_three_block: np.array) -> bool:
+    # could be done efficiently with diagonals
+    for _ in range(4):
+        diagonal_top = ''.join(np.diag(three_by_three_block, k=0))
+        flipped_up_three_by_three_block = np.flipud(three_by_three_block)
+        diagonal_bottom = ''.join(np.diag(flipped_up_three_by_three_block, k=0))
+
+        if diagonal_top == 'MAS' and diagonal_bottom == 'MAS':
+            return True
+
+        # rotate block 90 degrees
+        three_by_three_block = np.rot90(three_by_three_block)
+    return False
 
 
 if __name__ == '__main__':
     answer_a = run_a(day_file)
-    answer_b = run_b(test_file)
+    answer_b = run_b(day_file)
 
     console.print(f'solution A: {answer_a}')
     console.print(f'solution B: {answer_b}')
